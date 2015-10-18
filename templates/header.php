316365b9fcbae10ad93689a4a7fb20d3fa70cd25
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-18 14:08:48 +0700

    add header logo (reverted from commit f02c290b615d36ef5dbdd8144ade02cc589ca564)

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,14 @@
 <header class="banner">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-9 col-md-2 text-center">
-				<a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
-					<?php echo wp_get_attachment_image(3352,"full");?>
-				</a>
-			</div>
-			<div class="col-xs-3 col-md-offset-1 col-md-9">
-				<?php
-				if (has_nav_menu('primary_navigation')) :
-					wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-				endif;
-				?>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
+	    <?php get_attached_file();?>
+    </a>
+    <nav class="nav-primary">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </nav>
+  </div>
 </header>
