e122b0878558f9451930927b736a6af119a3c431
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-18 14:09:57 +0700

    fix frontpage

--- front-page.php
+++ front-page.php
@@ -1,14 +1,12 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 08121_000
+ * Date: 10/18/2015
+ * Time: 2:09 PM
+ */
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php the_posts_navigation(); ?>
+while (have_posts()) : the_post(); ?>
+	<?php get_template_part('templates/page', 'header'); ?>
+	<?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile;
