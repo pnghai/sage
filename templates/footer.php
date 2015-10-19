9f30c85684631d0e37d23fc30c8108dc3fdb728f
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-20 00:36:33 +0700

    fix tablet ui for footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,17 @@
 <footer class="content-info">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-md-3">
+      <div class="col-xs-12 col-sm-6 col-md-3">
         <?php dynamic_sidebar('sidebar-footer1'); ?>
       </div>
-      <div class="col-xs-12 col-md-3">
+      <div class="col-xs-12 col-sm-6 col-md-3">
         <?php dynamic_sidebar('sidebar-footer2'); ?>
       </div>
-      <div class="col-xs-12 col-md-2">
+      <div class="clearfix visible-sm-block"></div>
+      <div class="col-xs-12 col-sm-6 col-md-2">
         <?php dynamic_sidebar('sidebar-footer3'); ?>
       </div>
-      <div class="col-xs-12 col-md-offset-1 col-md-3">
+      <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-3">
         <?php dynamic_sidebar('sidebar-footer4'); ?>
       </div>
     </div>
