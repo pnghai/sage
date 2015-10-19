97b58be483834f297f953e0d04ccc3e8a1401b36
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-19 16:09:31 +0700

    fix header logo position on mobile

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,14 @@
 <header class="banner">
 	<div class="container">
 		<div class="row header-row">
-			<div class="col-xs-9 hidden-md hidden-lg text-center logo-brand">
+			<div class="col-xs-12 hidden-md hidden-lg text-center logo-brand">
 				<a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
 					<?php echo wp_get_attachment_image(3352,"full");?>
 				</a>
 			</div>
-			<div class="col-xs-3 col-md-12">
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
 				<?php
 				if (has_nav_menu('primary_navigation')) :
 					wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
