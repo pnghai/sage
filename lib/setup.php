45fe0c4b1292932b3b993aff49607f3cdd4556e1
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-20 22:16:28 +0700

    FIX THEME

--- lib/setup.php
+++ lib/setup.php
@@ -106,7 +106,7 @@ function assets() {
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
-function my_enqueue($hook) {
+function my_enqueue() {
   if (is_admin()) {
     wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
   }
