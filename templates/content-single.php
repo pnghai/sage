84c9e6101d412725ef91c57a97fef317aaca3753
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-25 15:31:40 +0700

    fix footer line-height (reverted from commit 613c05ec59888c096655bf50470677db2e1e8098) add single news

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,9 +1,11 @@
 <?php while (have_posts()) : the_post(); ?>
+  <h3 class="text-uppercase entry-categories"><?php _e('Category:','ug-2015'); the_category(', ');?></h3>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+      <time class="h5 updated text-uppercase entry-post-date" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+      <h1 class="text-uppercase entry-title"><?php the_title(); ?></h1>
     </header>
+    <figure class="feature-image"><?php the_post_thumbnail("full");?></figure>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
