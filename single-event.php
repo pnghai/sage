efb95eaf53269e6b05c3a3c03cf7e842762c5fc0
Hai Phan Nguyen <pnghai@gmail.com>
2015-10-25 21:57:15 +0700

    add template for event post type

---
+++ single-event.php
@@ -0,0 +1,1 @@
+<?php get_template_part('templates/content-single-event', get_post_type()); ?>
---
+++ templates/content-single-event.php
@@ -0,0 +1,17 @@
+<?php while (have_posts()) : the_post(); ?>
+  <h3 class="text-uppercase entry-categories"><?php _e('Category','ug-2015');?> : <? the_category(', ');?></h3>
+  <article <?php post_class(); ?>>
+    <header>
+      <time class="h5 updated text-uppercase entry-post-date" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+      <h1 class="text-uppercase entry-title"><?php the_title(); ?></h1>
+    </header>
+    <figure class="feature-image"><?php the_post_thumbnail("full");?></figure>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </footer>
+    <?php comments_template('/templates/comments.php'); ?>
+  </article>
+<?php endwhile; ?>
