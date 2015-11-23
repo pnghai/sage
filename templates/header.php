df0d540fbf52ad24b4d7c9b2d3f0528f6e1693ca
Hai Phan Nguyen <pnghai@gmail.com>
2015-11-23 18:25:13 +0700

    update header menu

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,14 @@
 <header class="banner">
 	<div class="container">
 		<div class="row header-row">
-			<div class="col-xs-12 hidden-md hidden-lg text-center logo-brand">
+			<div class="col-xs-12 text-center logo-brand">
+
+				<?php
+				if (has_nav_menu('primary_navigation')) :
+					wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+				endif;
+				?>
+				<?php display_search_box(DISPLAY_RESULTS_AS_POP_UP); ?>
 				<a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
 					<?php echo wp_get_attachment_image(3444,"full");?>
 				</a>
@@ -9,9 +16,4 @@
 		</div>
 
 	</div>
-	<?php
-	if (has_nav_menu('primary_navigation')) :
-	wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-	endif;
-	?>
 </header>
