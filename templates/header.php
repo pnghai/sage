704987d6292cbe2cfee11bc3d644560c32ed6823
Hai Phan Nguyen <pnghai@gmail.com>
2015-11-23 19:06:18 +0700

    fix header

--- templates/header.php
+++ templates/header.php
@@ -2,14 +2,17 @@
 	<div class="container">
 		<div class="row header-row">
 			<div class="col-xs-12 text-center">
-
 				<?php
 				if (has_nav_menu('primary_navigation')) :
 					wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
 				endif;
 				?>
+			</div>
+			<div class="col-xs-12 text-center searchbar">
 				<?php do_shortcode("[wp_google_searchbox]"); ?>
-				<a class="brand logo-brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
+			</div>
+			<div class="col-xs-12 text-center logo-brand">
+				<a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>">
 					<?php echo wp_get_attachment_image(3444,"full");?>
 				</a>
 			</div>
