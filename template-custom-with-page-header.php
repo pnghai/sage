ed2f82a447d45a5a87480018e00d8b1523f0c586
Hai Phan Nguyen <pnghai@gmail.com>
2015-11-24 10:49:29 +0700

    add templates for page

--- template-custom-with-page-header.php
+++ template-custom-with-page-header.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Template Name: Custom Template
+ * Template Name: Custom Template With Page Header
  */
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
