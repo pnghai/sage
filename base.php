e6e2a5bcabaf8607e57bd9d99d9739305f799763
Hai Phan Nguyen <pnghai@gmail.com>
2015-11-24 12:25:58 +0700

    fix base.php prepare for shop background

--- base.php
+++ base.php
@@ -14,7 +14,7 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <div class="content-wrapper">
+    <div class="container content-wrapper">
       <?php
         do_action('get_header');
         get_template_part('templates/header');
