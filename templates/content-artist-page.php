1b343d4ee90b5ec5815df41ba00a63efb7a6223f
Hai Phan Nguyen <pnghai@gmail.com>
2015-11-26 12:21:02 +0700

    finish artist navigation

--- templates/content-artist-page.php
+++ templates/content-artist-page.php
@@ -3,28 +3,27 @@
         <header>
             <h1 class="text-uppercase entry-title"><?php the_title(); ?></h1>
         </header>
-        <?php $parent_id = $post->post_parent;
-        $post = get_post($parent_id);
+        <?php
+        global $post;
+        $parent_id = $post->post_parent;
+        $artist_id = get_the_ID();
+        $parent = get_post($parent_id);
         $artists_page = get_page_by_title("Artists");
-        if ($post->post_parent == $artists_page->ID):
-            setup_postdata( $post );
-        endif;?>
+        $artist_id = ($parent->post_parent == $artists_page->ID)?$parent->ID:get_the_ID();
+        ?>
         <div class="post-nav">
             <ul>
-                <li><a href="<?php the_permalink();?>"><?php _e("Artist Biography","sage");?></a></li>
-                <?php $link = get_field("artist_exhibitions_link");
+                <li><a href="<?php echo get_permalink($artist_id);?>"><?php _e("Artist Biography","sage");?></a></li>
+                <?php $link = get_field("artist_exhibitions_link",$artist_id);
                 if ($link):?>
-                    <li><a href="<?php echo $link ?>">ARTIST EXHIBITIONS</a></li>
+                    <li><a href="<?php echo $link; ?>">ARTIST EXHIBITIONS</a></li>
                 <?php endif;?>
-                <?php $link = get_field("artist_related_items");
+                <?php $link = get_field("artist_related_items",$artist_id);
                 if ($link):?>
-                    <li><a hef="<?php echo $link;?>">SHOP ITEMS</a></li>
+                    <li><a href="<?php echo $link;?>">SHOP ITEMS</a></li>
                 <?php endif;?>
             </ul>
         </div>
-        <?php if ($post->post_parent == $artists_page->ID):
-            wp_reset_postdata();
-        endif;?>
     </div>
 
     <div class="col-md-7 col-sm-8">
