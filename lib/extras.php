eacac06322877b18bc94afe3e385f698f840dc5b
Hai Phan Nguyen <pnghai@gmail.com>
2015-12-13 21:18:57 +0700

    fix theme

--- lib/extras.php
+++ lib/extras.php
@@ -223,7 +223,7 @@ add_action('widgets_init', __NAMESPACE__ . '\\wpb_load_widget');
 
 add_filter('new_royalslider_skins', __NAMESPACE__ . '\\new_royalslider_add_custom_skin', 10, 2);
 function new_royalslider_add_custom_skin($skins) {
-    $skins['myCustomSkin'] = array(
+    $skins['rsLeftDefaultInv'] = array(
         'label' => 'Left Thumbs',
         'path' => get_stylesheet_directory_uri() . '/royalslider/left-default-inverted/rs-left-default-inverted.css'  // get_stylesheet_directory_uri returns path to your theme folder
     );
